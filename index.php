356e389bb28353cd859080b2abec4bb4f3cd8460
jwick6693 <70438604+jwick6693@users.noreply.github.com>
2020-09-04 00:36:58 +0530

    Update index.php

--- index.php
+++ index.php
@@ -2,12 +2,9 @@
 <html>
 <body>
 
-<h1>My first PHP page for UNNATI WEBHOOK v2.1</h1>
-  <marquee> Learning WEBHOOKS</marquee>
 <?php
-echo "Hello World! from GIT";
-echo "Hello from version1.1  ";
-echo " Why to Worry ? S2I in openshift";
+echo "Hello World!";
+echo "version1.1";
 ?>
 
 </body>
